88f0989cfb427322f991f4b4be58765532c19b72
Engkos <engkoskostaman97@gmail.com>
2019-07-14 02:25:23 +0700

    Tugas 4 javascripts
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas4.js/index.js
@@ -0,0 +1,16 @@
+function tinggibadansiswa(){
+  var nilai = 168;
+
+
+  if( nilai  === 168 ){
+    console.log("pendek")
+  }else if(nilai === 170){
+  console.log("sedang")
+}else if(nilai === 173){
+ console.log("tinggi")
+}else{
+   console.log("salah")
+  }
+
+}
+tinggibadansiswa()
